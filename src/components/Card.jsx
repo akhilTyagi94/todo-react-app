07a49b42c7a0b991f5af6b3e3cce05a254af34ca
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-05-30 23:39:56 +0530

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,56 @@
 import React from "react";
 
 const Card = ({ taskObj, index }) => {
+  const colors = [
+    {
+      primaryColor: "#5D93E1",
+      secondaryColor: "#ECF3FC",
+    },
+    {
+      primaryColor: "#F9D288",
+      secondaryColor: "#FEFAF1",
+    },
+    {
+      primaryColor: "#5DC250",
+      secondaryColor: "#F2FAF1",
+    },
+    {
+      primaryColor: "#F48687",
+      secondaryColor: "#FDF1f1",
+    },
+    {
+      primaryColor: "#B964F7",
+      secondaryColor: "#F3F0FD",
+    },
+  ];
+
   return (
-    <div class="card-wrapper mr-5">
-      <div class="card-top" style={{ "background-color": "chartreuse" }}></div>
+    <div class="card-wrapper">
+      <div
+        class="card-top"
+        style={{ "background-color": colors[index % 5].primaryColor }}
+      ></div>
       <div class="task-holder">
         <span
           class="card-header"
-          style={{ "background-color": "#F2FAF1", "border-radius": "10px" }}
+          style={{
+            "background-color": colors[index % 5].secondaryColor,
+            "border-radius": "10px",
+          }}
         >
           {taskObj.Name}
         </span>
-        <p>{taskObj.Description}</p>
+        <p className="mt-3">{taskObj.Description}</p>
 
         <div style={{ position: "absolute", right: "20px", bottom: "20px" }}>
-          <i class="far fa-edit mr-3" style={{ color: "#5DC250" }}></i>
-          <i class="fas fa-trash-alt" style={{ color: "#5DC250" }}></i>
+          <i
+            class="far fa-edit m-2"
+            style={{ color: colors[index % 5].primaryColor }}
+          ></i>
+          <i
+            class="fas fa-trash-alt"
+            style={{ color: colors[index % 5].primaryColor }}
+          ></i>
         </div>
       </div>
     </div>
